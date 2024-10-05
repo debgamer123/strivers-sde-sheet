0253ed54118561c89ce6560647adf933672cd675
debgamer123 <debayanari@gmail.com>
2024-10-05 14:17:21 +0530

    set-matrix-zeroes

--- src/set_matrix_zeroes.java
+++ src/set_matrix_zeroes.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class set_matrix_zeroes {
+import java.util.HashSet;
+import java.util.Set;
+
+public class set_matrix_zeroes {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> allRows = new HashSet<Integer>();
+        Set<Integer> allColumns = new HashSet<Integer>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    allRows.add(i);
+                    allColumns.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (allRows.contains(i) || allColumns.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }
